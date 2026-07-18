e55206d7b449ae410e6e878b70a42587986f3908
Awazii <115639025+awazii@users.noreply.github.com>
2026-07-18 08:36:54 +0500

    fixed the height problem of expense dashboard

--- src/Components/Expenses/Expensedetails/Expensedetails.jsx
+++ src/Components/Expenses/Expensedetails/Expensedetails.jsx
@@ -8,26 +8,27 @@ import { selectExpenseById } from '../../../store/ExpenseSlice'
 
 export const Expensedetails = ({ expenseid }) => {
     const Expense = useSelector(state => selectExpenseById(state, expenseid))
-    
+
     if (!Expense) return null;
 
     return (
         <div className='container mx-auto h-auto mt-2 flex flex-col lg:grid lg:grid-cols-5 gap-4'>
-            
+
             <div className="Overview lg:col-span-2 xl:col-span-3">
                 <Overview Expense={Expense} />
             </div>
-            
+
             <div className="insights lg:col-span-3 xl:col-span-2">
                 <Insights data={Expense.Members} />
             </div>
             <div className="Comparisan-graph bg-white shadow-md lg:col-span-3 rounded-lg p-2 h-[350px] md:h-[540px]">
                 <Comparisongraph Expense={Expense} />
             </div>
-            
-            <div className="Settlements lg:col-span-2 min-h-[300px]">
+
+            <div className="Settlements lg:col-span-2 h-[350px] md:h-[540px]">
                 <Settlements Expense={Expense} />
             </div>
+
         </div>
     )
 }
